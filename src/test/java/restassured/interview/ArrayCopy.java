fbca0931f1c849615d3a9910b7f389f0097f316a
mallikabhat99 <mallikabhat99@gmail.com>
2025-12-05 13:43:40 +0530

    copyofrange added

--- src/test/java/restassured/interview/ArrayCopy.java
+++ src/test/java/restassured/interview/ArrayCopy.java
@@ -13,9 +13,8 @@ public class ArrayCopy {
         int[] destinationArray2 = Arrays.copyOf(sourceArray, sourceArray.length);
         System.out.println(Arrays.toString(destinationArray2));
 
-        int[] destinationArray3 = sourceArray.clone();
+        int[] destinationArray3 = sourceArray.clone(); //shallow copy
         System.out.println(Arrays.toString(destinationArray3));
-
 
         int[] destinationArray4 = new int[sourceArray.length];
         for (int i = 0; i < sourceArray.length; i++) {
@@ -23,8 +22,29 @@ public class ArrayCopy {
         }
         System.out.println(Arrays.toString(destinationArray4));
 
+       // Shallow Copy:
+        // It copies only the reference of nested objects, not the actual data.
+        // Making changes to nested objects affect both the original and the copy.
+       // Deep Copy: It creates a full,
+        // independent copy of the object, including nested objects.
+        // Making any changes to the copy do not affect the original object.
+        int a[] = { 1, 8, 3, 5, 9, 10 };
 
+        // Creating an array b[] and
+        // copying elements of a[] to b[]
+        int b[] = Arrays.copyOfRange(a, 2, 6);
 
+        // Changing b[] to verify that
+        // b[] is different from a[]
+        b[0]++;  // Modify b[0] to check if it affects a[]
+
+        // Iterating over array a[]
+        System.out.println("aaaaaaaaaa\n");
+        System.out.println(Arrays.toString(a));
+
+        // Iterating over array b[]
+        System.out.println("bbbbbbbbbb\n");
+        System.out.println(Arrays.toString(b));
 
 
     }
